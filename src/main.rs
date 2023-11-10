67499e9ab01e9a76bded57bde1388a56f0fd096f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-11-11 08:38:42 +0900

    add hostname parameter

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,26 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        println!("Usage: {} <username> <password>", args[0]);
+    if args.len() < 4 {
+        println!("Usage: {} <hostname> <username> <password>", args[0]);
         return;
     }
 
     println!("args[0] : {}", args[0]);
     println!("args[1] : {}", args[1]);
     println!("args[2] : {}", args[2]);
+    println!("args[3] : {}", args[3]);
+
+    let hostname = &args[1];
+    let username = &args[2];
+    let password = &args[3];
 
     // Connect to the local SSH server
-    let tcp = TcpStream::connect("127.0.0.1:22").unwrap();
+    let tcp = TcpStream::connect(hostname.to_owned() + ":22").unwrap();
     let mut session = ssh2::Session::new().unwrap();
     session.set_tcp_stream(tcp);
     session.handshake().unwrap();
-    session.userauth_password(&args[1], &args[2]).unwrap();
+    session.userauth_password(username, password).unwrap();
     assert!(session.authenticated());
 
     let mut channel = session.channel_session().unwrap();
